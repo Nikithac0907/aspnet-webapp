94caa3f86b9ef4b50d7bb30b2e3c757b10783ae3
Tabish.Shaikh <tabish.shaikh@lntinfotech.com>
2020-01-09 12:31:01 +0530

    kd

--- repositoryapp/Global.asax.cs
+++ repositoryapp/Global.asax.cs
@@ -15,12 +15,17 @@ namespace repositoryapp
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-
-
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //hello there
+
+
+
+
+
+
+
         }
     }
 }
